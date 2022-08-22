3bda9edf9b658a9e4fa3d7e0a588e89ad1b0414d
arieldavis22 <amanichai100@gmail.com>
2022-08-22 17:00:37 -0400

    generics 2

--- src/GenericsClass/Main.java
+++ src/GenericsClass/Main.java
@@ -7,11 +7,27 @@ public class Main {
         BaseballPlayer tim = new BaseballPlayer("Tim");
         SoccerPlayer joe = new SoccerPlayer("Joe");
 
-        Team coolTeam = new Team("Cool Team");
+        Team<FootballPlayer> coolTeam = new Team<>("Cool Team");
         coolTeam.addPlayer(ariel);
-        coolTeam.addPlayer(tim);
-        coolTeam.addPlayer(joe);
+//        coolTeam.addPlayer(tim);
+//        coolTeam.addPlayer(joe);
 
         System.out.println(coolTeam.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Cubs");
+        baseballTeam.addPlayer(tim);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("Kick Team");
+        brokenTeam.addPlayer(joe);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gourdon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(coolTeam, 3, 8);
     }
 }
